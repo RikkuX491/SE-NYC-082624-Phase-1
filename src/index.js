41240220a7ea0f0d32a0e058e857bf0547d2d729
Ricardo Guerra <RikkuG491@gmail.com>
2024-08-26 17:58:16 -0400

    Added example code from the lecture

---
+++ src/index.js
@@ -0,0 +1,84 @@
+// console.log('hello')
+// alert('hello')
+// const personName = prompt("Please enter your name: ")
+// alert(`Welcome to Flatburger, ${personName}!`)
+
+// const num1 = Number(prompt("Enter your first number: "))
+// console.log(num1)
+// console.log(typeof num1)
+// const num2 = Number(prompt("Enter your second number: "))
+// console.log(num2)
+// console.log(typeof num2)
+// alert(`The sum of ${num1} + ${num2} is ${num1 + num2}`)
+
+// console.log(7 === 7)
+// console.log(7 === '7')
+// console.log(7 !== 7)
+// console.log(7 != '7')
+
+// Debugger examples
+// let personName = "Alice"
+// let age = 21
+// debugger
+// debugger
+// personName = "Bob"
+// debugger
+// age = age + 1
+// debugger
+
+const personName = "Alice"
+const age = 21
+const isStudent = "hello"
+
+// If, Else If, & Else Statements
+// let greetingPhrase
+
+// if(isStudent === true){
+//     greetingPhrase = `Hello ${personName}! Welcome to Flatiron School!`
+// }
+// else if(personName === "Alice"){
+//     greetingPhrase = "Hi Alice!"
+// }
+// else{
+//     greetingPhrase = "Goodbye!"
+// }
+
+
+// Same example with just If & Else statements
+// let greetingPhrase
+
+// if(isStudent === true){
+//     greetingPhrase = `Hello ${personName}! Welcome to Flatiron School!`
+// }
+// else{
+//     greetingPhrase = "Goodbye!"
+// }
+
+// Ternary operator
+// let greetingPhrase = isStudent === true ? `Hello ${personName}! Welcome to Flatiron School!` : "Goodbye!"
+
+// Switch statement
+// let greetingPhrase
+
+// switch(isStudent){
+//     case true:
+//         greetingPhrase = `Hello ${personName}! Welcome to Flatiron School!`
+//         console.log(1)
+//         break
+//     case false:
+//         greetingPhrase = "You are not a student!"
+//         console.log(2)
+//         break
+//     default:
+//         greetingPhrase = "Goodbye!"
+//         console.log(3)
+// }
+
+// console.log(greetingPhrase)
+
+// console.log(personName === "Alice" && age === 21)
+// console.log(personName === "Bob" && age === 21)
+// console.log(personName === "Bob" || age === 21)
+// console.log(!true)
+// console.log(!"hello")
+// console.log(!!"hello")
