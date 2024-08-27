9a1d3c76761890a3c4547c3dd64d20a2e27159fa
Ricardo Guerra <RikkuG491@gmail.com>
2024-08-27 18:02:47 -0400

    Updated example code from the lecture

--- src/index.js
+++ src/index.js
@@ -186,10 +186,68 @@ function cookFood(gatherIngredients){
 
 // cookFood(34)
 
-const cookedResultFunction = cookFood(() => {
-    console.log("Get onions...")
-    console.log("Get peppers...")
-    console.log("Get other ingredients...")
-})
+// const cookedResultFunction = cookFood(() => {
+//     console.log("Get onions...")
+//     console.log("Get peppers...")
+//     console.log("Get other ingredients...")
+// })
 
-cookedResultFunction()
+// cookedResultFunction()
+
+// Creating an array containing strings with names of foods
+const foods = ["pizza", "ice cream", "guacamole"]
+
+// Use the push() method to add an element to the end of the array
+foods.push("chicken")
+
+// Here's another way to add an element to the end of the array
+foods[foods.length] = "broccoli"
+
+// Use the unshift() method to add an element to the front of the array
+foods.unshift("ramen")
+
+// Use the pop() method to remove the very last element from the array
+foods.pop()
+
+// Use the shift() method to remove the very first element from the array
+foods.shift()
+
+console.log(foods)
+
+// Using the slice() method to get an array that contains a subset of data from the original array
+// console.log(foods.slice(1))
+// console.log(foods.slice(2))
+// console.log(foods.slice(3))
+// console.log(foods.slice(1, 3))
+
+// Destructively remove all elements after index 1 and including index 1 from the array stored in the foods variable
+// foods.splice(1)
+
+// Destructively remove 2 elements starting at index 1 and including index 1 from the array
+// foods.splice(1, 2)
+
+// Insert a new element at index 3 of the array (without removing any elements)
+// foods.splice(3, 0, "cheese")
+
+// Looping through an array using a for loop
+// for(let index = 0; index < foods.length; index++){
+//     console.log(foods[index])
+// }
+
+// Iterating through an array using a for...of loop
+// for(const food of foods){
+//     console.log(food)
+// }
+
+const animal = {
+    name: "Kitty",
+    animalType: "Cat",
+    isFluffy: true
+}
+
+console.log(animal)
+
+// for(const key in animal){
+//     console.log(key)
+//     console.log(animal[key])
+// }
