40942d5cc8a1fecd466fd76e1cfc22800e7d985a
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-06 11:50:07 -0400

    Added example code from the lecture

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const restaurantMenu = document.getElementById('restaurant-menu')
+const cryptocurrencyListElement = document.getElementById('cryptocurrency-list')
+const cryptocurrencySelectElement = document.getElementById('cryptocurrency-select')
+cryptocurrencySelectElement.addEventListener('change', handleChange)
+
+// A copy of the array containing the cryptocurrency objects (not initialized, but we will assign a value to it later)
+let cryptocurrencyArray
 
 function addFoodImageToRestaurantMenu(food){
     const imgElement = document.createElement('img')
@@ -34,6 +40,40 @@ function displayFoodDetails(food){
     foodDescriptionDisplayElement.textContent = food.description
 }
 
+function addCryptocurrencyListItemToList(cryptocurrency){
+    // console.log(cryptocurrency)
+    const liElement = document.createElement('li')
+
+    // [name] ([symbol]): Rank # [rank] - each <li> should have data in this format
+    liElement.textContent = `${cryptocurrency.name} (${cryptocurrency.symbol}): Rank # ${cryptocurrency.rank}`
+    
+    cryptocurrencyListElement.appendChild(liElement)
+}
+
+function handleChange(event) {
+    // console.log(event.target.value)
+
+    cryptocurrencyListElement.innerHTML = ""
+
+    if(event.target.value === 'all'){
+        cryptocurrencyArray.forEach(addCryptocurrencyListItemToList)
+    }
+    else if(event.target.value === 'less_than'){
+        cryptocurrencyArray.forEach(cryptocurrency => {
+            if(parseInt(cryptocurrency.rank) <= 50){
+                addCryptocurrencyListItemToList(cryptocurrency)
+            }
+        })
+    }
+    else if(event.target.value === 'greater_than'){
+        cryptocurrencyArray.forEach(cryptocurrency => {
+            if(parseInt(cryptocurrency.rank) > 50){
+                addCryptocurrencyListItemToList(cryptocurrency)
+            }
+        })
+    }
+}
+
 fetch('http://localhost:3000/foods')
 .then(response => response.json())
 .then(foods => {
@@ -44,4 +84,21 @@ fetch('http://localhost:3000/foods')
     })
 })
 
-// write your code here
+fetch("https://api.coincap.io/v2/assets")
+.then(response => response.json())
+.then(apiData => {
+    // console.log(apiData)
+    // console.log(apiData.data)
+    
+    // Filter implementation from Deliverable # 3
+    // const filteredCryptocurrencies = apiData.data.filter(cryptocurrency => {
+    //     return parseInt(cryptocurrency.rank) <= 10
+    // })
+
+    // console.log(filteredCryptocurrencies)
+    // console.log(apiData.data)
+
+    cryptocurrencyArray = apiData.data
+
+    cryptocurrencyArray.forEach(addCryptocurrencyListItemToList)
+})
